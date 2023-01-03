a641f1693ca9a7dcdf79a00c9310ec38e4a1cd54
PabloAbreu1 <pagustin2404@gmail.com>
2023-01-03 17:50:58 +0000

    randomcards

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    let palo = ["♣", "♠", "♥", "♦"];
+    let number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+    let sorteoPalo = Math.floor(Math.random() * palo.length);
+    let sorteoNumber = Math.floor(Math.random() * number.length);
+
+    let contenedorNumber = number[sorteoNumber];
+    let contenedorPalo = palo[sorteoPalo];
+
+    let color = contenedorPalo == "♦" || contenedorPalo == "♥" ? "red" : "black";
+
+    document.getElementById('arriba').innerHTML = contenedorPalo;
+    document.getElementById('arriba').style.color = color;
+    document.getElementById('medio').innerHTML = contenedorNumber;
+    document.getElementById('medio').style.color = color;
+    document.getElementById('abajo').innerHTML = contenedorPalo;
+    document.getElementById('abajo').style.color = color;
+
+};
